81645c24758e5159c2a8daaa6ecb150ef022e712
wagnerxciv <125404234+wagnerxciv@users.noreply.github.com>
2023-03-31 17:46:59 -0300

    modificaçao

--- galitos/contato.php
+++ galitos/contato.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro agenda</title>
+    <title>Busca na agenda</title>
 </head>
 <body>
-    <h1>Cadastro Agenda</h1>
+    <h1>Busca na agenda</h1>
 
     <!-- Formulário HTML -->
 <form action="" method="post">
