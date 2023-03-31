101ad20ab84ed51c60e5cf884ae6e67f28b8a087
wagnerxciv <125404234+wagnerxciv@users.noreply.github.com>
2023-03-31 17:30:12 -0300

    modificaçao

--- galitos/contato.php
+++ galitos/contato.php
@@ -9,9 +9,9 @@
 <body>
 <form>
   <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
+  <input type="text" id="fname" name="fname" value="wagner"><br>
   <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe">
+  <input type="text" id="lname" name="lname" value="willian">
 </form>
     <h1>  CADASTRO DE CONTATOS </h1>
     <form action="" method="post">
